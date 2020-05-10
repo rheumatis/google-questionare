811e33076cf63752691a6be54d0461c55b1bcd6e
rheumatis <61785695+rheumatis@users.noreply.github.com>
2020-05-11 00:01:45 +0900

    Add files via upload
    현재 마지막 최종파일 입니다. 파이참에서는 잘 구동이 됩니다. 그러나 검색 후 pyinstaller module 설치 후로 exe 파일로 성공적으로 변환하였으나 실행이 안되는 것 같습니다. 예를 들면 exe 실행 후 크롬 브라우저 열어 해당 켬퓨터 IP 넣어도 실행이 안됩니다. AWS에서 flask 구동시키는 방법으로 가야하는지 ..

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ scope = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive',
 ]
-json_file_name = 'absolute-water-275404-2c5754f0926c.json'
+json_file_name = '../sparta/absolute-water-275404-2c5754f0926c.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
 gc = gspread.authorize(credentials)
 # 공유 링크
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/16XeeQXR4bMLEhT_rN74EQsXLONbGo4RPs_MMbb6eS40/edit?usp=sharing'
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/16XeeQXR4bMLEhT_rN74EQsXLONbGo4RPs_MMbb6eS40/edit#gid=1012218114'
 doc = gc.open_by_url(spreadsheet_url)
 worksheet = doc.worksheet('Form Responses 1')
 
@@ -20,7 +20,7 @@ app = Flask(__name__)
 worksheet.update_acell('U1', '')
 
 cell_list = worksheet.range('A1:U1')
-cell_values = ['A. Timestamp','B. 이름','C. 생년월일','D. 진료참고자료제출','S. Positive ROS','F. MHx','H. Drug SE/allergy','G. Op Hx','J. Health Exam Hx','K. Smoking','L. Drinking','M. Regular exercise','N. Job','O. Marrige','P. Offspring','Q. Spont. Abortion','R. Menopause', 'E. 실비서류', 'T. Others', 'I. FHx']
+cell_values = ['A. Timestamp','B. 이름','C. 생일','D. 확인사항','S. Positive ROS','F. MHx','H. Drug SE/allergy','G. Op Hx','J. Health Exam','K. Smoking','L. Drinking','M. Work Out','N. Occupation','O. Marrige','P. Offspring','Q. Abortion','R. Menopause', 'E. 실비서류', 'T. Others', 'I. FHx']
 
 for i, val in enumerate(cell_values):
         cell_list[i].value = val
